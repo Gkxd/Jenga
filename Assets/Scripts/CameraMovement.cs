28f6af109d2b0ff5e92417622f3802d384618aa8
Gkxd <dhua@andrew.cmu.edu>
2015-09-11 22:52:35 -0400

    Basic Jenga setup

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform cameraTransform;
+
+    public float rotationSpeed;
+    public float zoomSpeed;
+
+    private float yaw = -45;
+    private float pitch = 0;
+
+    private float scale = 1;
+
+    void Start() {
+        cameraTransform.transform.LookAt(transform);
+    }
+
+    void Update() {
+        
+        if (Input.GetMouseButton(1)) {
+            yaw += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            pitch -= Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        }
+
+        scale -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime;
+        scale = Mathf.Max(scale, 0.1f);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+        transform.localScale = Vector3.one * scale;
+    }
+}
---
+++ Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBuilder : MonoBehaviour {
+
+    public GameObject blockPrefab;
+    public int numberOfLayers;
+    public float jitter;
+
+    void Start() {
+        float blockHeight = blockPrefab.transform.localScale.y;
+        float blockWidth = blockPrefab.transform.localScale.x;
+
+        for (int i = 0; i < numberOfLayers; i++) {
+            float layerHeight = blockHeight * i;
+
+            Vector3 position = new Vector3(0, layerHeight, 0);
+
+            Quaternion layerRotation = Quaternion.Euler(Vector3.up * (90 * (i % 2)));
+            Vector3 offset = layerRotation * new Vector3(blockWidth, 0, 0);
+
+            for (int j = -1; j <= 1; j++) {
+                Vector3 randomness = jitter * (offset * Random.Range(-0.1f, 0.1f) + layerRotation * offset * Random.Range(-0.2f, 0.2f));
+
+                GameObject block = (GameObject)Object.Instantiate(blockPrefab, position + j * offset + randomness, layerRotation);
+                block.transform.parent = transform;
+            }
+        }
+    }
+}
