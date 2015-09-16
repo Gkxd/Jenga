f9b32c8b2afff09df5d204a5d35438c2d92d0892
swethakan <swetha@cmu-883824.wv.cc.cmu.edu>
2015-09-16 11:08:21 -0400

    Color Added
    red and black cubes

---
+++ Assets/Scripts/ColorCodingAcceleration.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCodingAcceleration : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody rb;
+	public GameObject thisBlock;
+	public bool selected =	false;
+	
+	//public bool selected = MouseGesture.selected;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (selected == false) {
+			if (rb.velocity.z != 0.0) {
+
+				MeshRenderer blockRenderer = GetComponent<MeshRenderer> ();
+				blockRenderer.material.SetColor ("_Color", new Color (1-rb.velocity.z, 1-rb.velocity.z, 1-rb.velocity.z));
+		
+			} else {
+				changeColor ();
+
+			}
+		} else {
+			MeshRenderer blockRenderer = GetComponent<MeshRenderer> ();
+
+			blockRenderer.material.SetColor ("_Color", new Color (1,0,0));
+
+		}
+	
+	}
+
+	void changeColor(){
+
+				
+		MeshRenderer blockRenderer = GetComponent<MeshRenderer> ();
+		blockRenderer.material.SetColor ("_Color", new Color(1,1,1));
+
+	}
+
+}
