f0317b89c8abc59233a9fe01809aabe05988515a
Gkxd <dhua@andrew.cmu.edu>
2015-09-25 17:28:14 -0400

    Added Jenga Leapmotion test scene

--- Assets/Scripts/Controls/ShowHelpScreen.cs
+++ Assets/Scripts/Controls/ShowHelpScreen.cs
@@ -3,22 +3,10 @@ using System.Collections;
 
 public class ShowHelpScreen : MonoBehaviour {
 
-	public GameObject helpScreen;
-
-	// Use this for initialization
-	void Start () {
-		helpScreen.GetComponent<Canvas> ().enabled = false;
+    [Header("Reference Settings")]
+    public Canvas helpScreenCanvas;
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("h")) {
-			helpScreen.GetComponent<Canvas>().enabled = true;
-		}
-		if (Input.GetKeyUp("h")) {
-			helpScreen.GetComponent<Canvas>().enabled = false;
-		}
-	
+        helpScreenCanvas.enabled = Input.GetKey(KeyCode.H);
 	}
 }
