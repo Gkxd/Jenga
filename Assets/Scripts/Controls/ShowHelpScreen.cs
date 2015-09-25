eb91e9f4d711b04baf34c06f72e0c774bae677bd
swethakan <swetha@cmu-883824.wv.cc.cmu.edu>
2015-09-24 22:30:21 -0400

    Instruction added on pressing 'h'

---
+++ Assets/Scripts/Controls/ShowHelpScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHelpScreen : MonoBehaviour {
+
+	public GameObject helpScreen;
+
+	// Use this for initialization
+	void Start () {
+		helpScreen.GetComponent<Canvas> ().enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("h")) {
+			helpScreen.GetComponent<Canvas>().enabled = true;
+		}
+		if (Input.GetKeyUp("h")) {
+			helpScreen.GetComponent<Canvas>().enabled = false;
+		}
+	
+	}
+}
